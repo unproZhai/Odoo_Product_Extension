89eae782cd6e9c74008d6c53e8b4e2af70784962
unproZhai <42622194+unproZhai@users.noreply.github.com>
2019-09-12 13:11:45 -0400

    init

---
+++ __manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "productExtension",
+
+    'summary': """
+        Adding item number and model number to the product""",
+
+    'description': """
+        Adding item number and model number to the product
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'product','sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ models/models.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class ProductProduct(models.Model):
+	_inherit = "product.product"
+	item_number = fields.Char(index=True, help="Item Number", copy=False)
+	model_number = fields.Char(index=True, help="Model Number", copy=False)
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    item_number = fields.Char(store=True, index=True,
+                               related='product_variant_ids.item_number',
+                               readonly=False, help='Item Number')
+
+    model_number = fields.Char(store=True, index=True,
+                               related='product_variant_ids.model_number',
+                               readonly=False, help='Model Number')
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    item_number = fields.Char('Item Number', related='product_variant_ids.item_number')
+    model_number = fields.Char('Model Number', related='product_variant_ids.model_number')
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    item_number = fields.Char('Item Number', related='product_variant_ids.item_number')
+    model_number = fields.Char('Model Number', related='product_variant_ids.model_number')
