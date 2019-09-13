eaf51149ac84af78a765db2c59fa148e3a8a1bcc
unproZhai <42622194+unproZhai@users.noreply.github.com>
2019-09-13 11:21:46 -0400

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,6 @@
     """,
 
     'author': "My Company",
-    'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
